90cc21acd58d809311d5e1af10a6817b58eb0bdc
Lightninggabe1 <lightninggabe191@gmail.com>
2025-03-24 11:09:12 +0800

    Update Linear Regression.py
    3/13

--- Linear Regression/Linear Regression.py
+++ Linear Regression/Linear Regression.py
@@ -33,3 +33,27 @@ y = [m*x + b for x in months]
 plt.plot(months, revenue, "o")
 plt.plot(months,y)
 plt.show()
+
+#3/13: Loss
+x = [1, 2, 3]
+y = [5, 1, 3]
+
+#y = x
+m1 = 1
+b1 = 0
+
+#y = 0.5x + 1
+m2 = 0.5
+b2 = 1
+
+y_predicted1 = [m1*x_value + b1 for x_value in x]
+y_predicted2 = [m2*x_value + b2 for x_value in x]
+total_loss1 = 0
+for i in range(len(y)):
+  total_loss1 += (y[i]-y_predicted1[i])**2
+total_loss2 = 0
+for i in range(len(y)):
+  total_loss2 += (y[i]-y_predicted2[i])**2
+print(total_loss1)
+print(total_loss2)
+better_fit = 2
