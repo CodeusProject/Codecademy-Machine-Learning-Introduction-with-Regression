579600d3d084e6575c94b03c4198c3734ec2d1fe
Lightninggabe1 <lightninggabe191@gmail.com>
2025-04-04 13:51:18 +0800

    Update Linear Regression.py
    6/13

--- Linear Regression/Linear Regression.py
+++ Linear Regression/Linear Regression.py
@@ -69,3 +69,24 @@ def get_gradient_at_b(x, y, m, b):
     diff += (y_value - ((m * x_value) + b))
   b_gradient = (-2/len(x))*diff
   return b_gradient
+
+#6/13: Gradient Descent for Slope
+def get_gradient_at_b(x, y, m, b):
+    diff = 0
+    N = len(x)
+    for i in range(N):
+      y_val = y[i]
+      x_val = x[i]
+      diff += (y_val - ((m * x_val) + b))
+    b_gradient = -2/N * diff
+    return b_gradient
+
+def get_gradient_at_m(x,y,m,b):
+  diff = 0
+  U = len(x)
+  for i in range(U):
+    y_val = y[i]
+    x_val = x[i]
+    diff += x_val*(y_val - (m*x_val+b))
+  m_gradient = diff * (-2/U)
+  return m_gradient
