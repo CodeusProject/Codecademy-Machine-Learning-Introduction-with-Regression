f9fe85196f3dec971af0e33f199cb7a9a3bc54eb
Lightninggabe1 <lightninggabe191@gmail.com>
2025-04-04 14:35:05 +0800

    Update Linear Regression.py
    10/13

--- Linear Regression/Linear Regression.py
+++ Linear Regression/Linear Regression.py
@@ -213,4 +213,20 @@ plt.plot(months, y)
 
 plt.show()
 
+#11/13: Use Your Functions on Real Data
+import codecademylib3_seaborn
+from gradient_descent_funcs import gradient_descent
+import pandas as pd
+import matplotlib.pyplot as plt
 
+df = pd.read_csv("heights.csv")
+
+X = df["height"]
+y = df["weight"]
+
+plt.plot(X, y, 'o')
+#plot your line here:
+b, m = gradient_descent(X, y, num_iterations = 1000, learning_rate = 0.0001)
+y_predictions = [X_val*m + b for X_val in X]
+plt.plot(X, y_predictions)
+plt.show()
