a8e5c672246945bf7a06cec2bfddb27f12af08c5
Lightninggabe1 <lightninggabe191@gmail.com>
2025-04-04 22:02:10 +0800

    Update Linear Regression.py
    13/13 Review

--- Linear Regression/Linear Regression.py
+++ Linear Regression/Linear Regression.py
@@ -247,3 +247,33 @@ sales_predict = line_fitter.predict(temperature)
 plt.plot(temperature, sales, 'o')
 plt.plot(temperature, sales_predict)
 plt.show()
+
+#13/13: Review
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_boston
+
+# Boston housing dataset
+boston = load_boston()
+
+df = pd.DataFrame(boston.data, columns = boston.feature_names)
+
+# Set the x-values to the nitrogen oxide concentration:
+X = df[['NOX']]
+# Y-values are the prices:
+y = boston.target
+
+# Can we do linear regression on this?
+
+
+
+
+plt.scatter(X, y, alpha=0.4)
+# Plot line here:
+
+plt.title("Boston Housing Dataset")
+plt.xlabel("Nitric Oxides Concentration")
+plt.ylabel("House Price ($)")
+plt.show()
