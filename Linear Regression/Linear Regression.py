0a14f22bc857c0ded40ddb31031a9f72a9964680
Lightninggabe1 <lightninggabe191@gmail.com>
2025-04-04 21:53:53 +0800

    Update Linear Regression.py
    12/13

--- Linear Regression/Linear Regression.py
+++ Linear Regression/Linear Regression.py
@@ -230,3 +230,20 @@ b, m = gradient_descent(X, y, num_iterations = 1000, learning_rate = 0.0001)
 y_predictions = [X_val*m + b for X_val in X]
 plt.plot(X, y_predictions)
 plt.show()
+
+#12/13: Scikit-Learn
+import codecademylib3_seaborn
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import numpy as np
+
+temperature = np.array(range(60, 100, 2))
+temperature = temperature.reshape(-1, 1)
+sales = [65, 58, 46, 45, 44, 42, 40, 40, 36, 38, 38, 28, 30, 22, 27, 25, 25, 20, 15, 5]
+line_fitter = LinearRegression()
+line_fitter.fit(temperature, sales)
+sales_predict = []
+sales_predict = line_fitter.predict(temperature)
+plt.plot(temperature, sales, 'o')
+plt.plot(temperature, sales_predict)
+plt.show()
