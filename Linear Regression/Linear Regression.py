e470cfa9b748f1ecb58e27b09a03d57fdcd652bd
Lightninggabe1 <lightninggabe191@gmail.com>
2025-04-04 14:17:28 +0800

    Update Linear Regression.py
    9/13

--- Linear Regression/Linear Regression.py
+++ Linear Regression/Linear Regression.py
@@ -144,3 +144,17 @@ plt.ylabel("b value")
 plt.show()
 num_iterations = 800
 convergence_b = 48
+
+#9/13: Learning Rate
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+from data import bs, bs_000000001, bs_01
+
+iterations = range(100)
+
+plt.plot(iterations, bs_01)
+plt.xlabel("Iterations")
+plt.ylabel("b value")
+plt.show()
+num_iterations = 800
+convergence_b = 48
