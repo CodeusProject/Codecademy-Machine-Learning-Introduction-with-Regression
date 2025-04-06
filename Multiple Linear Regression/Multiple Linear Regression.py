1a01e3898f1b9bff48d9c04137cbe032022717e5
Lightninggabe1 <lightninggabe191@gmail.com>
2025-04-06 15:58:42 +0800

    Update Multiple Linear Regression.py
    3/10

--- Multiple Linear Regression/Multiple Linear Regression.py
+++ Multiple Linear Regression/Multiple Linear Regression.py
@@ -41,7 +41,7 @@ ax.set_xlabel('Size (ft$^2$)')
 ax.set_ylabel('Building Age (Years)')
 ax.set_zlabel('Rent ($)')
 
-ax.w_xaxis.set_ticklabels([])
+ax.w_xaxis.set_ticklabels([]) 
 ax.w_yaxis.set_ticklabels([])
 ax.w_zaxis.set_ticklabels([])
 
@@ -53,3 +53,23 @@ import codecademylib3_seaborn
 import pandas as pd
 df = pd.read_csv("https://raw.githubusercontent.com/Codecademy/datasets/master/streeteasy/manhattan.csv")
 print(df.head())
+
+#3/10: Training Set vs. Test Set
+import codecademylib3_seaborn
+import pandas as pd
+
+# import train_test_split
+from sklearn.model_selection import train_test_split
+
+streeteasy = pd.read_csv("https://raw.githubusercontent.com/sonnynomnom/Codecademy-Machine-Learning-Fundamentals/master/StreetEasy/manhattan.csv")
+
+df = pd.DataFrame(streeteasy)
+
+x = df[['bedrooms', 'bathrooms', 'size_sqft', 'min_to_subway', 'floor', 'building_age_yrs', 'no_fee', 'has_roofdeck', 'has_washer_dryer', 'has_doorman', 'has_elevator', 'has_dishwasher', 'has_patio', 'has_gym']]
+y = df[['rent']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state = 6)
+print(x_train.shape)
+print(x_test.shape)
+print(y_train.shape)
+print(y_test.shape)
