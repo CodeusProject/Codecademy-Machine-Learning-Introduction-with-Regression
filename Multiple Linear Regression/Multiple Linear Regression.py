7dd5958b96659f84862e257468bc5f1501addc9c
Lightninggabe1 <lightninggabe191@gmail.com>
2025-04-07 14:58:03 +0800

    Update Multiple Linear Regression.py
    8/10

--- Multiple Linear Regression/Multiple Linear Regression.py
+++ Multiple Linear Regression/Multiple Linear Regression.py
@@ -185,3 +185,34 @@ plt.scatter(df[['min_to_subway']], df[['rent']], alpha=0.4)
 plt.scatter(df[['floor']], df[['rent']], alpha=0.2)
 plt.scatter(df[['building_age_yrs']], df[['rent']], alpha=0.1)
 plt.show()
+
+#8/10: Evaluating the Model's Accuracy
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+streeteasy = pd.read_csv("https://raw.githubusercontent.com/sonnynomnom/Codecademy-Machine-Learning-Fundamentals/master/StreetEasy/manhattan.csv")
+
+df = pd.DataFrame(streeteasy)
+
+x = df[['bedrooms', 'bathrooms', 'size_sqft', 'min_to_subway', 'floor', 'building_age_yrs', 'no_fee', 'has_roofdeck', 'has_washer_dryer', 'has_doorman', 'has_elevator', 'has_dishwasher', 'has_patio', 'has_gym']]
+
+y = df[['rent']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state=6)
+
+mlr = LinearRegression()
+
+model=mlr.fit(x_train, y_train)
+
+y_predict = mlr.predict(x_test)
+
+# Input code here:
+print("Train score:")
+print(mlr.score(x_train, y_train))
+
+print("Test score:")
+print(mlr.score(x_test, y_test))
